b63bf1711694b145eef37cc1cdd390f22db8f848
ViperEvo <speed6767@wp.pl>
2019-06-18 19:08:58 +0200

    sass changed to scss and so on

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.sass']
+  styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
 
