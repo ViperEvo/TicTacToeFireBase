39cdb5e67397302e20d145339d27932b1cb2d42c
ViperEvo <speed6767@wp.pl>
2019-06-19 21:00:07 +0200

    Timer works, now send it to database

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -18,15 +18,21 @@ export class BoardComponent implements OnInit {
   squares = Array(9).fill(null);
   player = 'X';
   winner = '';
-
+  xTime = 0;
+  xRunning = 0;
+  xSecs = 0;
+  xTenths = 0;
+  oTime = 0;
+  oRunning = 0;
+  oSecs = 0;
+  oTenths = 0;
   @Output() winnerEmit = new EventEmitter<string>();
 
   newGame() {
     if (this.player === 'X')
     {
       this.player = 'X';
-    }
-    else
+    } else
     {
       this.player = 'O';
     }
@@ -42,7 +48,60 @@ export class BoardComponent implements OnInit {
         this.winnerEmit.emit(this.player);
       }
       this.player = this.player === 'X' ? 'O' : 'X';
+      if (this.player === 'X')
+      {
+        this.oRunning = 1;
+        this.oTimerStartPause();
+        this.xTimerStartPause();
+      } else
+      {
+        this.xRunning = 1;
+        this.xTimerStartPause();
+        this.oTimerStartPause();
+      }
     }
+  }
+  xTimerStartPause() {
+    if (this.xRunning === 0) {
+      this.xRunning = 1;
+      this.xIncrement();
+    } else
+    {
+      this.xRunning = 0;
+    }
+  }
+  xIncrement() {
+    setTimeout(() => {
+      if (this.xRunning === 1)
+      {
+        this.xTime += 1;
+        this.xSecs = Math.floor(this.xTime / 10);
+        this.xTenths = this.xTime % 10;
+        document.getElementById('playerXTime').innerHTML = 'X player timer : ' +  this.xSecs + ':' + this.xTenths;
+        this.xIncrement();
+      }
+    }, 100);
+  }
+  oTimerStartPause() {
+    if (this.oRunning === 0) {
+      this.oRunning = 1;
+      this.oIncrement();
+    } else
+    {
+      this.oRunning = 0;
+    }
+  }
+  oIncrement() {
+    setTimeout(() => {
+      if (this.oRunning === 1)
+      {
+        this.oTime += 1;
+        this.oSecs = Math.floor(this.oTime / 10);
+        this.oTenths = this.oTime % 10;
+        document.getElementById('playerOTime').innerHTML = 'O player timer : ' + this.oSecs + ':' + this.oTenths;
+        this.oIncrement();
+      }
+    }, 100);
   }
 
   winMove(): boolean {
@@ -57,6 +116,5 @@ export class BoardComponent implements OnInit {
   }
 
   ngOnInit() {
-
   }
 }
