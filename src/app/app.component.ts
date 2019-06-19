b2edbb35e5df81b9c77283b3cd987ec0bbcd8f79
ViperEvo <speed6767@wp.pl>
2019-06-19 22:01:57 +0200

    Firebase works. now CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import { Observable } from 'rxjs';
 
 interface Score {
   winner: string;
-  time: string;
+  xsecs: string;
+  xtenths: string;
+  osecs: string;
+  otenths: string;
 }
 
 @Component({
@@ -17,7 +20,12 @@ export class AppComponent {
 
   scoreCol: AngularFirestoreCollection<Score>;
   scores: Observable<Score[]>;
-
+  fData = null;
+  winner = null;
+  xsecs = null;
+  xtenths = null;
+  osecs = null;
+  otenths = null;
   constructor(private afs: AngularFirestore) {
   }
 
@@ -25,7 +33,11 @@ export class AppComponent {
     this.scoreCol = this.afs.collection('Scores');
     this.scores = this.scoreCol.valueChanges();
   }
-  addScore(winner) {
-    this.afs.collection('Scores').add({'winner': winner, 'time': '0'});
+  addScore(fireJson) {
+    console.log(fireJson);
+    this.fData = JSON.parse(fireJson);
+    console.log(this.fData['xSecs']);
+
+    this.afs.collection('Scores').add({'winner': this.fData['Winner'], 'xsecs': this.fData['xSecs'], 'xtenths': this.fData['xTenths'], 'osecs': this.fData['oSecs'], 'otenths': this.fData['oTenths']});
   }
 }
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,6 +1,7 @@
 import { SquareComponent } from './../square/square.component';
 import { Component, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { JsonPipe } from '@angular/common';
 
 
 @Component({
@@ -26,6 +27,8 @@ export class BoardComponent implements OnInit {
   oRunning = 0;
   oSecs = 0;
   oTenths = 0;
+  fireJson = '';
+  fireData = null;
   @Output() winnerEmit = new EventEmitter<string>();
 
   newGame() {
@@ -38,6 +41,16 @@ export class BoardComponent implements OnInit {
     }
     this.squares = Array(9).fill(null);
     this.winner = null;
+    this.xTime = 0;
+    this.xRunning = 0;
+    this.xSecs = 0;
+    this.xTenths = 0;
+    this.oTime = 0;
+    this.oRunning = 0;
+    this.oSecs = 0;
+    this.oTenths = 0;
+    document.getElementById('playerXTime').innerHTML = 'X player timer : ' +  this.xSecs + ':' + this.xTenths;
+    document.getElementById('playerOTime').innerHTML = 'O player timer : ' +  this.oSecs + ':' + this.oTenths;
   }
 
   makeMove(position) {
@@ -45,7 +58,13 @@ export class BoardComponent implements OnInit {
       this.squares[position] = this.player;
       if (this.winMove()){
         this.winner = this.player;
-        this.winnerEmit.emit(this.player);
+        this.oRunning = 0;
+        this.xRunning = 0;
+        this.oTimerStartPause();
+        this.xTimerStartPause();
+        this.fireData = { Winner : this.player, xSecs : this.xSecs, xTenths : this.xTenths, oSecs : this.oSecs, oTenths : this.oTenths};
+        this.fireJson = JSON.stringify(this.fireData);
+        this.winnerEmit.emit(this.fireJson);
       }
       this.player = this.player === 'X' ? 'O' : 'X';
       if (this.player === 'X')
@@ -108,9 +127,8 @@ export class BoardComponent implements OnInit {
     const lines = [
       [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
     ];
-    for (let line of lines) {
-      if (this.squares[line[0]] && this.squares[line[0]] === this.squares[line[1]] && this.squares[line[1]] === this.squares[line[2]])
-      { return true;}
+    for (const line of lines) {
+      if (this.squares[line[0]] && this.squares[line[0]] === this.squares[line[1]] && this.squares[line[1]] === this.squares[line[2]]) { return true; }
     }
     return false;
   }
